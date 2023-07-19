5ccb1ee98c90e906de6151af63cb334b2b2b8e78
sqrtNOT <sqrtnot@users.noreply.github.com>
2023-07-19 01:41:02 -0700

    Add script for downloading and extracting data files

---
+++ dl_gsotd.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/sh
+wget -P archive https://www.ncei.noaa.gov/data/global-summary-of-the-day/archive/{1929..2023}.tar.gz
+for i in {1929..1929}
+do
+	mkdir archive/$i
+	tar -xvf archive/$i.tar.gz -C archive/$i/
+done
