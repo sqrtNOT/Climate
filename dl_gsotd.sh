cab218dfb972e9bdaa9c109cfed17c8c8b7fa9af
sqrtNOT <sqrtnot@users.noreply.github.com>
2023-07-19 01:56:25 -0700

    fix ranges

--- dl_gsotd.sh
+++ dl_gsotd.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/sh
 wget -P archive https://www.ncei.noaa.gov/data/global-summary-of-the-day/archive/{1929..2023}.tar.gz
-for i in {1929..1929}
+for i in {1929..2023}
 do
 	mkdir archive/$i
 	tar -xvf archive/$i.tar.gz -C archive/$i/
